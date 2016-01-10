98db8c01b0b0b738933d02e881dbd24e5380cbb7
Agustin Barto <abarto@gmail.com>
2016-01-10 00:03:43 -0300

    Set-up admin for blogposts models

--- full_text_search_drf/blogposts/admin.py
+++ full_text_search_drf/blogposts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blogpost, Comment
+
+
+class BlogpostAdmin(admin.ModelAdmin):
+    fields = ('title', 'slug', 'description', 'content', 'allow_comments', 'author', 'created', 'modified')
+    readonly_fields = ('slug', 'created', 'modified', 'author')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    fields = ('blogpost', 'content', 'author', 'created', 'modified')
+    readonly_fields = ('created', 'modified', 'blogpost', 'author')
+
+
+admin.site.register(Blogpost, BlogpostAdmin)
+admin.site.register(Comment, CommentAdmin)
